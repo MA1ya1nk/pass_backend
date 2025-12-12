3855925d5ab8c5199832544e0e91c40fd101774d
Mayank <mk20040307@gmail.com>
2025-12-12 18:32:04 +0530

    fixing bugs

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,6 +1,11 @@
 import { Router } from "express";
 import { login, Register, logout, addPassword, deletePassword, updatePassword } from "../controllers/user.controller.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const router = Router()
 
@@ -20,5 +25,10 @@ router.route("/me").get(verifyJWT, async (req, res) => {
 router.route("/addPassword").post(verifyJWT, addPassword)
 router.route("/deletePassword").post(verifyJWT, deletePassword)
 router.route("/updatePassword").put(verifyJWT, updatePassword)
+
+router.use(express.static(path.join(__dirname, "../frontend/dist")));
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
 
 export default router
